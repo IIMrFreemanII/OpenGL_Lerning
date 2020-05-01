16a844de16a9e144736b858725cd9d79c8c4e6a2
IIMrFreemanII <kdeagovec@gmail.com>
2020-05-01 23:24:21 +0300

    draw rectangle

--- main.cpp
+++ main.cpp
@@ -103,7 +103,7 @@ int main()
          0.5f,  0.5f, 0.0f,  // top right
          0.5f, -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+        -0.5f,  0.5f, 0.0f   // top left
     };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
