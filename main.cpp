1214b279b5a931efe4242b7389be901f74e1ebd6
IIMrFreemanII <kdeagovec@gmail.com>
2020-05-01 23:36:20 +0300

    refactor indices

--- main.cpp
+++ main.cpp
@@ -109,6 +109,8 @@ int main()
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
     };
+    int indicesAmount = sizeof(indices) / sizeof(indices[0]);
+
     unsigned int VBO, VAO, EBO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
@@ -156,7 +158,7 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, indicesAmount, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
