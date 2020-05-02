3d2d5e8fc312fa23fe30a90fac1ed2589f95dd0f
IIMrFreemanII <kdeagovec@gmail.com>
2020-05-02 22:43:15 +0300

    add staging files

---
+++ cmake-build-debug/CMakeFiles/OpenGL_GLFL_Lerning.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/OpenGL_GLFW_Libs/GLAD/src/glad.c" "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/cmake-build-debug/CMakeFiles/OpenGL_GLFL_Lerning.dir/OpenGL_GLFW_Libs/GLAD/src/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../OpenGL_GLFW_Libs/GLAD/include"
+  "../OpenGL_GLFW_Libs/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/src/main.cpp" "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/cmake-build-debug/CMakeFiles/OpenGL_GLFL_Lerning.dir/src/main.cpp.obj"
+  "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/src/textures/TextureLoader.cpp" "E:/VisualStudioProjects/OpenGL_GLFL_Lerning/cmake-build-debug/CMakeFiles/OpenGL_GLFL_Lerning.dir/src/textures/TextureLoader.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../OpenGL_GLFW_Libs/GLAD/include"
+  "../OpenGL_GLFW_Libs/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGL_GLFL_Lerning.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_GLFL_Lerning.dir/OpenGL_GLFW_Libs/GLAD/src/glad.c.obj"
+  "CMakeFiles/OpenGL_GLFL_Lerning.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGL_GLFL_Lerning.dir/src/textures/TextureLoader.cpp.obj"
+  "OpenGL_GLFL_Lerning.exe"
+  "OpenGL_GLFL_Lerning.exe.manifest"
+  "OpenGL_GLFL_Lerning.lib"
+  "OpenGL_GLFL_Lerning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_GLFL_Lerning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
